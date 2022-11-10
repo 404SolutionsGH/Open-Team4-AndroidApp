d3161c3e5fd3fb52b9b0e7e740ec5ed86fb8c4d8
Koose <KooseTheBlogger@gmail.com>
2022-11-10 13:28:06 +0000

    splash screen activity xml with the trotro logo

---
+++ app/src/main/java/com/koose/open_team4androidapp/Screens/SplashScreen.kt
@@ -0,0 +1,12 @@
+package com.koose.open_team4androidapp.Screens
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.koose.open_team4androidapp.R
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
