4031650ce3451a79dbc6462f55f3fe42d183e6df
Koose <KooseTheBlogger@gmail.com>
2022-11-10 13:42:15 +0000

    Splash screens works

--- app/src/main/java/com/koose/open_team4androidapp/Screens/SplashScreen.kt
+++ app/src/main/java/com/koose/open_team4androidapp/Screens/SplashScreen.kt
@@ -1,12 +1,32 @@
 package com.koose.open_team4androidapp.Screens
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.WindowManager
+import com.koose.open_team4androidapp.MainActivity
 import com.koose.open_team4androidapp.R
 
+@Suppress("DEPRECATION")
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        // This is used to hide the status bar and make
+        // the splash screen as a full screen activity.
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        // Handler().postDelayed({
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000) // 3000 is the delayed time in milliseconds.
     }
 }
