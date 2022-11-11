19356dd52ef19ceabc8d4bb6e1f80d7308e0812c
Koose <KooseTheBlogger@gmail.com>
2022-11-11 17:34:52 +0000

    Working onBoarding Screen

---
+++ app/src/main/java/com/koose/open_team4androidapp/screens/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.koose.open_team4androidapp.screens
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.koose.open_team4androidapp.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
--- app/src/main/java/com/koose/open_team4androidapp/screens/SplashScreen.kt
+++ app/src/main/java/com/koose/open_team4androidapp/screens/SplashScreen.kt
@@ -1,4 +1,4 @@
-package com.koose.open_team4androidapp.Screens
+package com.koose.open_team4androidapp.screens
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
@@ -6,7 +6,6 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.view.WindowManager
-import com.koose.open_team4androidapp.Screens.OnboardingScreen
 import com.koose.open_team4androidapp.R
 
 @Suppress("DEPRECATION")
