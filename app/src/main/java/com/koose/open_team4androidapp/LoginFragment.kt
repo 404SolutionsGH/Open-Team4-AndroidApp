1530a664cffe7821737a830850e7b70459c00ac4
Koose <KooseTheBlogger@gmail.com>
2022-11-16 16:02:52 +0000

    Register page going well. Needs to prune

--- app/src/main/java/com/koose/open_team4androidapp/LoginFragment.kt
+++ app/src/main/java/com/koose/open_team4androidapp/LoginFragment.kt
@@ -1,22 +1,45 @@
 package com.koose.open_team4androidapp
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.koose.open_team4androidapp.databinding.FragmentLoginBinding
 
 
 class LoginFragment : Fragment() {
 
+
+    private lateinit var loginBinding: FragmentLoginBinding
 
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        //inflate the layout for this fragment
+        loginBinding = FragmentLoginBinding.inflate(inflater, container, false)
+
+        loginBinding.signupText.setOnClickListener {
+            onClick()
+        }
+
+        return loginBinding.root
+
+    }
+
+    private fun onClick() {
+        findNavController().navigate(
+            LoginFragmentDirections.actionLoginFragmentToRegisterFragment()
+        )
+    }
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        loginBinding
     }
 
 
